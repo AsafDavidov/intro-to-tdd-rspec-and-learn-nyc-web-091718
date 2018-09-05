fc069922e0a7c47e6c93dc6f3fac10b00225bb63
Asaf Davidov <adavidov@conncoll.edu>
2018-09-05 15:23:31 +0000

    automatically backed up by learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -1,4 +1,4 @@
-//
+=begin
 require_relative '../current_age_for_birth_year.rb'
 
 describe "current_age_for_birth_year method" do
@@ -8,7 +8,7 @@ describe "current_age_for_birth_year method" do
     expect(age_of_person).to eq(19)
   end
 end
-//
+=end
 
 
 require_relative '../current_age_for_birth_year.rb'
